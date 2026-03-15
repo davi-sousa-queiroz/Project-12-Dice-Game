ef886ce145982072ed31f7ff2742b5523330ba5f
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-15 19:17:39 -0300

    Commit roll function

--- Dice Game.py
+++ Dice Game.py
@@ -12,4 +12,11 @@ money = 250
 # ----------------- FUNCTIONS -------------------
 def guess_number():
     guess = input("What's your guess? (1-6): ")
+
+def roll():
+    print("Rolling dice...")
+    time.sleep(1)
+    dice_number = random.randint(1, 6)
+    print("You rolled " + str(dice_number))
+    time.sleep(1)
 # -------------- MAIN GAME LOOP -----------------
