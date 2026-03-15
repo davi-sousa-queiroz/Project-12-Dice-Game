ae3bd4ff162310c084b31ea91cb1c0ad0b720895
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-15 19:56:09 -0300

    Commit completion of main function, game, and loop.

--- Dice Game.py
+++ Dice Game.py
@@ -33,3 +33,19 @@ def check_guess(guess):
         money -= 75
         print("-75$")
 # -------------- MAIN GAME LOOP -----------------
+def main():
+    intro()
+    while money > 0 and money < 1000:
+        guess = int(input("What's your guess? (1-6): "))
+        check_guess(guess)
+        time.sleep(1)
+        print(f"Current money: {money}$")
+    if money > 1000:
+        print("You passed 1000$!")
+        print("Great job!")
+        time.sleep(1)
+    else:
+        print("You ran out of money!")
+        time.sleep(1)
+if __name__ == "__main__":
+    main()
