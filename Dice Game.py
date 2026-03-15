1cea1aa4202aaef221beb8dbaf25702cdab751c1
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-15 19:23:17 -0300

    Commit intro transferred into a function

--- Dice Game.py
+++ Dice Game.py
@@ -4,6 +4,14 @@ import random
 # -------------- GAME VARIABLES ----------------
 money = 250
 # ----------------- FUNCTIONS -------------------
+def intro():
+    print("Welcome to Dice Game! 🎲")
+    time.sleep(1)
+    print("Make 1000$ to win! 🤑")
+    time.sleep(1)
+    print("Go bankrupt to lose 😓💼...")
+    time.sleep(1)
+
 def guess_number():
     guess = input("What's your guess? (1-6): ")
 
