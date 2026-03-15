8891b525b918b884b23562bc8557bc3887f1b2ff
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-15 19:19:45 -0300

    Commit removal of game intro to transfer into a function

--- Dice Game.py
+++ Dice Game.py
@@ -1,12 +1,6 @@
 # ----------- IMPORTS AND GAME INTRO ------------
 import time
 import random
-print("Welcome to Dice Game! 🎲")
-time.sleep(1)
-print("Make 1000$ to win! 🤑")
-time.sleep(1)
-print("Go bankrupt to lose 😓💼...")
-time.sleep(1)
 # -------------- GAME VARIABLES ----------------
 money = 250
 # ----------------- FUNCTIONS -------------------
